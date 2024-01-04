1f70edf111200cc49b649f900663be59949aed79
Samara Vitória <devsamaravitoria@gmail.com>
2024-01-04 14:14:48 -0300

    fix: Declaração da instância

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-Iphone celularUm = new Iphone("81983649752", "Iphone 8", "356080093924233", 256);
+Smartphone celularUm = new Iphone("81983649752", "Iphone 8", "356080093924233", 256);
+Console.WriteLine("Smartphone Iphone:");
 celularUm.InstalarAplicativo("DIO");
 celularUm.Ligar();
-celularUm.ReceberLigacao();
 
-Nokia celularDois = new Nokia("81992569335", "Windows Phone", "356080095975431", 256);
+Smartphone celularDois = new Nokia("81992569335", "Windows Phone", "356080095975431", 256);
+Console.WriteLine("Smartphone Nokia:");
 celularDois.InstalarAplicativo("DIO");
-celularDois.Ligar();
 celularDois.ReceberLigacao();
