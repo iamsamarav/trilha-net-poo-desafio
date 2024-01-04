5b679ad8c05ab08cae6c61bbf89db60486d55e83
Samara Vitória <devsamaravitoria@gmail.com>
2024-01-04 14:10:28 -0300

    feat: Modificações finais

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone celularUm = new Iphone("81983649752", "Iphone 8", "356080093924233", 256);
+celularUm.InstalarAplicativo("DIO");
+celularUm.Ligar();
+celularUm.ReceberLigacao();
+
+Nokia celularDois = new Nokia("81992569335", "Windows Phone", "356080095975431", 256);
+celularDois.InstalarAplicativo("DIO");
+celularDois.Ligar();
+celularDois.ReceberLigacao();
